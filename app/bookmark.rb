6292e820d36c22180d26d86833189c08846d9f4f
elizabethvenner <elizabethvenner@googlemail.com>
2016-10-18 12:42:59 +0100

    creating a modular sinatra app

--- app/bookmark.rb
+++ app/bookmark.rb
@@ -1,9 +1,11 @@
 require 'sinatra/base'
+require_relative 'models/link'
 
 class Bookmarks < Sinatra::Base
 
-  get '/' do
-    erb :index
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
   end
 
   # start the server if ruby file executed directly
--- spec/show_link_spec.rb
+++ spec/show_link_spec.rb
@@ -1,11 +1,12 @@
-require_relative './../app/models/link'
+require './app/models/link'
 
 feature 'show links on homepage' do
   scenario 'show a link' do
     link = Link.create(url: 'example.com', title: 'link')
-    visit '/'
+    visit '/links'
     # expect(page.status_code).to eq 200
-
-    expect(page).to have_content 'link'
+    within 'ul#links' do
+      expect(page).to have_content 'Title: link'
+    end
   end
 end
